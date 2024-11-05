6b15e376d3f83853e3589a244448a1986b962409
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2024-11-05 16:01:25 +0100

    implement Address and Hotel Entities

--- src/main/java/tn/esb/siad/smart_hotel_backend/Models/Address.java
+++ src/main/java/tn/esb/siad/smart_hotel_backend/Models/Address.java
@@ -1,4 +1,22 @@
 package tn.esb.siad.smart_hotel_backend.Models;
 
+import jakarta.persistence.*;
+import lombok.NonNull;
+
+import java.util.Objects;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    @NonNull
+    private String street;
+    private String city;
+    @NonNull
+    private String state;
+    private String country;
+    @NonNull
+    private String zipCode;
+
 }
--- src/main/java/tn/esb/siad/smart_hotel_backend/Models/Hotel.java
+++ src/main/java/tn/esb/siad/smart_hotel_backend/Models/Hotel.java
@@ -1,26 +1,29 @@
 package tn.esb.siad.smart_hotel_backend.Models;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import tn.esb.siad.smart_hotel_backend.Enumerations.HotelRating;
-@Getter
-@Setter
-@AllArgsConstructor
-@RequiredArgsConstructor
-@ToString
-@EqualsAndHashCode
+@Getter //generates all getters methods for private or protected fields
+@Setter //generates all setters methods for private or protected fields
+@AllArgsConstructor //generates a constructor with all fields as arguments
+@RequiredArgsConstructor //generates a constructor with only required fields as arguments (fields annotated with NonNull)
+@ToString //generates redefinition of toString() method (returns value of all fields)
+//@EqualsAndHashCode //generates redefinition of equals and hashCode methods using all fields
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true) //only fields annotated with @EqualsAndHashCode.Include are used to compare objects
+@EqualsAndHashCode(of="name") //compare Hotel objects by name
 @Entity
 public class Hotel {
-    @Id
-    @GeneratedValue
+    @Id //primary key
+    //id is auto-generated
+    //IDENTITY to say that the first inserted Hotel has the id = 1, the second has id=2, ...
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    //@EqualsAndHashCode.Include
     @Column(length=100,unique = true)
     @NonNull //the name is required
     private String name;
+    @Enumerated(EnumType.STRING)
     private HotelRating rating;
 
 }
